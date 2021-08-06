3dcb4d95f15e5704db584040b9f9c2739a70f894
naerim <naerim1119@gmail.com>
2021-08-06 17:27:55 +0900

    naerim/ Add ViewPager 슬라이더

---
+++ app/src/main/java/com/example/summerdrawer/CustomPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.summerdrawer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.util.ArrayList;
+
+public class CustomPagerAdapter extends PagerAdapter {
+    Context context;
+    ArrayList<Integer> pager;
+
+    public CustomPagerAdapter(Context context, ArrayList<Integer> pager) {
+        this.context = context;
+        this.pager = pager;
+    }
+
+    @Override
+    public int getCount() {
+        return pager.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return view == o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.pager_item, container, false);
+        RoundedImageView imageView = (RoundedImageView) view.findViewById(R.id.image);
+        imageView.setCornerRadius(20);
+        imageView.setBackgroundResource(pager.get(position));
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return super.getItemPosition(object);
+    }
+}
--- app/src/main/java/com/example/summerdrawer/MainActivity.java
+++ app/src/main/java/com/example/summerdrawer/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.summerdrawer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    ArrayList<Integer> arrayList;
+    LinearLayout layout_dot;
+    TextView[] dot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        layout_dot = (LinearLayout) findViewById(R.id.layout_dot);
+        arrayList = new ArrayList<>();
+
+        arrayList.add(R.color.red);
+        arrayList.add(R.color.green);
+        arrayList.add(R.color.colorPrimaryDark);
+        arrayList.add(R.color.colorAccent);
+
+        CustomPagerAdapter pagerAdapter = new CustomPagerAdapter(getApplicationContext(), arrayList);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setPageMargin(50);
+        addDot(0);
+
+        // whenever the page changes
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+            @Override
+            public void onPageSelected(int i) {
+                addDot(i);
+            }
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+    }
+
+    public void addDot(int page_position) {
+        dot = new TextView[arrayList.size()];
+        layout_dot.removeAllViews();
+
+        for (int i = 0; i < dot.length; i++) {;
+            dot[i] = new TextView(this);
+            dot[i].setText(Html.fromHtml("&#9679;"));
+            dot[i].setTextSize(10);
+            dot[i].setTextColor(getResources().getColor(R.color.darker_gray));
+            layout_dot.addView(dot[i]);
+        }
+        //active dot
+        dot[page_position].setTextColor(getResources().getColor(R.color.red));
     }
 }
