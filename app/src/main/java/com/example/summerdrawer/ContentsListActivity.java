8f50f5319806a7e6d244911311afa899df8d1f6e
지인 <sonia9.shin@gmail.com>
2021-08-17 13:20:09 +0900

    jiin/ 컨텐츠리스트 레이아웃 수정

--- app/src/main/java/com/example/summerdrawer/ContentsListActivity.java
+++ app/src/main/java/com/example/summerdrawer/ContentsListActivity.java
@@ -10,31 +10,16 @@ import android.widget.TextView;
 
 public class ContentsListActivity extends AppCompatActivity {
 
-    ImageButton btn_goProfile_list, btn_goSearch_lst;
-    Button btn_logo_lst;
-    TextView categoryNameTxt, sortByTxt;
-    TextView issueContentsTitleTxt, issueContentsDescTxt;
-    ImageView issueContentsImg, setSortByImg;
+    Button btn_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contents_list);
 
-        //좌측 상단의 프로필 버튼을 누르면 왼쪽에 네비게이션 드로어 그려주기
-        btn_goProfile_list = findViewById(R.id.btn_goProfile_list);
-        btn_goProfile_list.setOnClickListener(view->{
+        btn_logo = findViewById(R.id.btn_logo);
+        btn_logo.setText(getIntent().getStringExtra("content")+"서랍");
 
-        });
-
-        //우측 상단의 돋보기 버튼을 누르면 검색 액티비티로 이동
-        btn_goSearch_lst = findViewById(R.id.btn_goSearch_lst);
-        btn_goSearch_lst.setOnClickListener(view->{
-
-        });
-
-        categoryNameTxt = findViewById(R.id.categoryNameTxt);
-        categoryNameTxt.setText(getIntent().getStringExtra("content"));
 
     }
 }
