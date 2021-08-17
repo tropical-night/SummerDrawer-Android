99e078ce5a5fce84c8134f66af1903ec6cba3009
지인 <sonia9.shin@gmail.com>
2021-08-17 13:19:51 +0900

    jiin/ 네비게이션 바 클릭 이벤트 구현

--- app/src/main/java/com/example/summerdrawer/MainActivity.java
+++ app/src/main/java/com/example/summerdrawer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.summerdrawer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.CompositePageTransformer;
@@ -15,6 +16,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -29,12 +31,14 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<SliderItems> sliderItems;
     ArrayList<RankItems> rankItems;
     WormDotsIndicator dots_indicator;
-    ViewPager2 viewPager2_recycler;
-    TabLayout tabLayout;
 
     ImageButton btn_goProfile;
     private DrawerLayout drawerLayout;
     private View drawerView;
+
+    ConstraintLayout toLike, toScrap, toMovie, toBook, toWebtoon, toDrama, toMagazine;
+    TextView userNameTxt, toLikeTxt, toScrapTxt,
+            toMovieTxt, toBookTxt, toWebtoonTxt, toDramaTxt, toMagazineTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +75,81 @@ public class MainActivity extends AppCompatActivity {
                 // 클릭시 이벤트
             }
         });
+
+        //사용자 이름 받아와서 설정해주기
+        userNameTxt = findViewById(R.id.userNameTxt);
+        userNameTxt.setText("김뫄뫄");
+
+        //좋아하는 버튼 클릭 시
+        toLike = findViewById(R.id.toLike);
+        toLikeTxt = findViewById(R.id.toLikeTxt);
+        //좋아하는 작품으로 액티비티 이동
+        toLike.setOnClickListener(view->{
+        });
+        toLikeTxt.setOnClickListener(view->{
+        });
+
+        //저장해둔 버튼 클릭 시
+        toScrap = findViewById(R.id.toLike);
+        toScrapTxt = findViewById(R.id.toLikeTxt);
+        //저장해둔 작품으로 액티비티 이동
+        toLike.setOnClickListener(view->{
+        });
+        toLikeTxt.setOnClickListener(view->{
+        });
+
+        //영화 버튼 클릭시
+        toMovie = findViewById(R.id.toMovie);
+        toMovieTxt = findViewById(R.id.toMovieTxt);
+        //영화 추천 리스트 액티비티로 이동
+        toMovie.setOnClickListener(view->{
+            toContentsList("영화");
+        });
+        toMovieTxt.setOnClickListener(view->{
+            toContentsList("영화");
+        });
+
+        //도서 버튼 클릭시
+        toBook = findViewById(R.id.toBook);
+        toBookTxt = findViewById(R.id.toBookTxt);
+        //도서 추천 리스트 액티비티로 이동
+        toBook.setOnClickListener(view->{
+            toContentsList("도서");
+        });
+        toBookTxt.setOnClickListener(view->{
+            toContentsList("도서");
+        });
+
+        //웹툰 버튼 클릭시
+        toWebtoon = findViewById(R.id.toWebtoon);
+        toWebtoonTxt = findViewById(R.id.toWebtoonTxt);
+        //도서 추천 리스트 액티비티로 이동
+        toWebtoon.setOnClickListener(view->{
+            toContentsList("웹툰");
+        });
+        toWebtoonTxt.setOnClickListener(view->{
+            toContentsList("웹툰");
+        });
+
+        //드라마 버튼 클릭시
+        toDrama = findViewById(R.id.toDrama);
+        toDramaTxt = findViewById(R.id.toDramaTxt);
+        //드라마 추천 리스트 액티비티로 이동
+        toDrama.setOnClickListener(view->{
+            toContentsList("드라마");
+        });
+        toDramaTxt.setOnClickListener(view->{
+            toContentsList("드라마");
+        });
+
+        //읽을거리 버튼 클릭시
+        toMagazine = findViewById(R.id.toMagazine);
+        toMagazineTxt = findViewById(R.id.toMagazineTxt);
+//        //읽을거리 리스트 액티비티로 이동
+//        toDrama.setOnClickListener(view->{
+//        });
+//        toDramaTxt.setOnClickListener(view->{
+//        });
 
         // slider item 추가
         sliderItems = new ArrayList<>();
