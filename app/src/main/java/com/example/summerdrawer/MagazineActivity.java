5825bbf6f47fd86de3e91c539aa9ad0c7084f23c
지인 <sonia9.shin@gmail.com>
2021-08-20 05:02:30 +0900

    jiin/ 매거진 만들다 말았음

---
+++ app/src/main/java/com/example/summerdrawer/MagazineActivity.java
@@ -0,0 +1,14 @@
+package com.example.summerdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MagazineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magazine);
+    }
+}
